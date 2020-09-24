9bc167d81c6adf65916cd7eb0fe7bee4ada31665
Alexa <alexakearns@outlook.com>
2020-09-24 09:59:18 +0100

    Create dashboard component with  Another World

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Dashboard from './components/dashboard'
 
 class App extends Component {
 
   render() {
     return(
       <div>
-        <h1>Hello World!</h1>
+        <Dashboard />
       </div>
     )
   }
