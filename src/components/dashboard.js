ad09b3ba6c4d3e0ead3f3aa0e134adc68bbe87df
Alexa <alexakearns@outlook.com>
2020-09-24 15:12:00 +0100

    Add nasa component to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Nasa from './nasa';
+import Weather from './weather'
+import '../app.css'
 
 class Dashboard extends Component {
 
   render() {
     return(
       <div>
+        <Container>
+          <h1 id='greeting'>Alexa's Daily Dashboard</h1>
+        </Container>
+        <Container>
+          <Row>
+            <Col>
+              <Nasa />
+            </Col>
+            <Col>
+              <Weather />
+            </Col>
+          </Row>
+        </Container>
         
       </div>
     )
